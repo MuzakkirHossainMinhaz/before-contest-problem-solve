b93a1d04d4dad77a467243605d6d872b433152fe
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-01 07:52:51 +0600

    Area of Circular

---
+++ 3. Area of Circular.cpp
@@ -0,0 +1,26 @@
+/**
+    Calculate the area of a circular.
+    Formula: A = PI * r^2
+    PI = 3.14159
+
+    Input: Radius: 5
+    Output: Area: 78.5397
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float radius, area;
+    cout << "Radius: ";
+    cin >> radius;
+
+    float PI = 3.14159;    /// PI = 2*acos(0);
+
+    area = PI * radius * radius;
+
+    cout << "Area: " << area << endl;
+
+    return 0;
+}
