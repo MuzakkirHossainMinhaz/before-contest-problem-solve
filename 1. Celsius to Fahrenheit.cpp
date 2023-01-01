a98bb894163439e4ec258305cde388a7e4c0493a
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-01 07:15:18 +0600

    first commit

---
+++ 1. Celsius to Fahrenheit.cpp
@@ -0,0 +1,24 @@
+/**
+    Enter the input in Celsius and find the output Fahrenheit.
+    Formula: C/5 = (F-32)/9
+          => F = ((C*9)/5 + 32)
+
+    Input: Celsius: 5
+    Output: Fahrenheit: 41
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float celsius;
+    cout << "Celsius: ";
+    cin >> celsius;
+
+    float fahrenheit = (celsius*9)/5 + 32;
+
+    cout << "Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
