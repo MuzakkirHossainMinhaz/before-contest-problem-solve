746aed69ccef19a5b2d31aa0481b3ea109fcb400
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-02 15:07:24 +0600

    A Given Number is Even or Odd

--- 14. A Given Number is Even or Odd.cpp
+++ 14. A Given Number is Even or Odd.cpp
@@ -1,5 +1,5 @@
 /**
-    A Given Number is Even or Odd.
+    Take Input a Number from User and Check Whether the Number is Even or Odd.
     Hints: every even number is divisible by 2.
 
     Input: 3332
