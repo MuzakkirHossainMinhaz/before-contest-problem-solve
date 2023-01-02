c61a72be66311011367acdbfa45fd72ec2c1f5e4
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-02 15:00:19 +0600

    A Given Year is Leap Year or Not

---
+++ 15. A Given Year is Leap Year or Not.cpp
@@ -0,0 +1,22 @@
+/**
+    A Given Year is Leap Year or Not.
+
+    Input: 2023
+    Output: Not a Leap Year
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+
+    if((year%4==0 and year%100!=0) or (year%400==0))
+        cout << "Leap Year" << endl;
+    else
+        cout << "Not a Leap Year" << endl;
+
+    return 0;
+}
