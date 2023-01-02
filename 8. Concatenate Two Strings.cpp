f7f1722c40abfccb50e63a298ba27215808c4b12
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-02 09:31:00 +0600

    Concatenate Two Strings

---
+++ 8. Concatenate Two Strings.cpp
@@ -0,0 +1,21 @@
+/**
+    Concatenate Two Strings.
+
+    Input: abc xyz
+    Output: abcxyz
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    string result = str1+str2;
+
+    cout << result << endl;
+
+    return 0;
+}
