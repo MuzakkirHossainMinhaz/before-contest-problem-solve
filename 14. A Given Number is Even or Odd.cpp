86bdcf58d1a685e8826ced25a12398d9a7293ea5
MuzakkirHossainMinhaz <minhaz.bauet@gmail.com>
2023-01-02 14:59:17 +0600

    A Given Number is Even or Odd

---
+++ 14. A Given Number is Even or Odd.cpp
@@ -0,0 +1,23 @@
+/**
+    A Given Number is Even or Odd.
+    Hints: every even number is divisible by 2.
+
+    Input: 3332
+    Output: Even
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    if(number%2==0)
+        cout << "Even" << endl;
+    else
+        cout << "Odd" << endl;
+
+    return 0;
+}
